e1304c16094f18958f4a864420236e0fb603ef28
Vu Tran <baysao@gmail.com>
2022-12-29 13:31:16 +0700

    chore add install option

--- mbr.sh
+++ mbr.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 DIR=$(realpath $(dirname $0))
-if [ $# -eq 0 ];then
-   	git clone git@github.com:massbitprotocol/mbr_gbc.git
+if [ $# -eq 0 ]; then
+	git clone git@github.com:massbitprotocol/mbr_gbc.git
 	git clone git@github.com:massbitprotocol/mbr_app.git
 	cp mbr_app/mbr.sh .
 	exit 0
 fi
-   
+
 case $1 in
+update)
+	git -C mbr_gbc pull
+	git -C mbr_app pull
+	;;
 run)
 	docker run --rm -d --privileged -v $DIR/mbr_gbc:/tmp/gbc -v $DIR/mbr_app:/tmp/app --name mbr_app -p 80:80 -p 443:443 baysao/openresty:alpine /tmp/gbc/prepare_alpine.sh /tmp/app
 	docker ps
@@ -18,4 +22,3 @@ reload)
 	docker exec -it mbr_app /tmp/app/cmd_server status
 	;;
 esac
-
