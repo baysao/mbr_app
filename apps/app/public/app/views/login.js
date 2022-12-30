69b4cb2a0baba1818790fb21dd7c1f93a4eb7710
TomPham204 <thect04@gmail.com>
2022-12-30 17:46:31 +0700

    Update login form

--- apps/app/public/app/views/login.js
+++ apps/app/public/app/views/login.js
@@ -8,89 +8,163 @@ define([], function () {
 		},
 		elements: [
 			{
+				css: "px-2",
 				cols: [
+					{ gravity: 1 },
 					{
-						cols: [
+						gravity: 2,
+						rows: [
+							{
+								cols: [
+									{
+										cols: [
+											{
+												view: "template",
+												template:
+													'<img id="icon" src="assets/images/logo/logo.png" alt="MBR logo"></img>',
+												css: "icon",
+												gravity: 1,
+											},
+											{
+												view: "label",
+												label: "MassbitRoute",
+												css: "no-border",
+												gravity: 3,
+											},
+										],
+										gravity: 1,
+									},
+
+									{
+										view: "label",
+										label: "Docs",
+										css: "no-border",
+										align: "right",
+										gravity: 2,
+									},
+								],
+							},
+							{ gravity: 0.5 },
 							{
 								view: "template",
-								template:
-									'<img id="icon" src="assets/images/logo/logo.png" alt="MBR logo"></img>',
-								css: "icon",
-								gravity: 1,
+								template: "<h1 id='register-header'>Login</h1>",
+								align: "center",
+								height: 70,
+								borderless: true,
 							},
 							{
-								view: "label",
-								label: "MassbitRoute",
-								css: "no-border",
-								gravity: 3,
+								view: "button",
+								id: "register",
+								css: "no-bg webix_transparent",
+								value: "Don’t have an account? Sign Up",
+								align: "center",
+								borderless: true,
 							},
+							{ view: "text", id: "email", label: "Email" },
+							{
+								view: "text",
+								id: "password",
+								type: "password",
+								label: "Password",
+							},
+							{
+								cols: [
+									{ gravity: 2 },
+									{
+										view: "button",
+										id: "reset-password",
+										value: "Forgot Password",
+										css: "webix_transparent",
+										align: "right",
+										gravity: 1,
+									},
+								],
+							},
+							{
+								cols: [
+									{
+										view: "button",
+										id: "login",
+										value: "Login",
+										css: "webix_primary",
+										align: "center",
+									},
+								],
+							},
+							{
+								cols: [
+									{
+										view: "button",
+										id: "with_google",
+										value: "Google",
+										css: "webix_transparent no-bg",
+										gravity: 1,
+										css: "center",
+									},
+									{ gravity: 0.2 },
+									{
+										view: "button",
+										id: "with_github",
+										value: "Github",
+										css: "webix_transparent no-bg",
+										align: "center",
+										gravity: 1,
+									},
+								],
+							},
+							{ gravity: 1 },
 						],
-						gravity: 1,
 					},
-
-					{
-						view: "label",
-						label: "Docs",
-						css: "no-border",
-						align: "right",
-						gravity: 2,
-					},
-				],
-			},
-			{
-				view: "template",
-				template: "<h1 id='register-header'>Login</h1>",
-				align: "center",
-				height: 80,
-				borderless: true,
-			},
-			{ view: "text", id: "email", label: "Email" },
-			{
-				view: "text",
-				id: "password",
-				type: "password",
-				label: "Password",
-			},
-			{
-				cols: [
-					{
-						view: "button",
-						id: "register",
-						value: "Register",
-						css: "webix_primary",
-						align: "center",
-					},
-				],
-			},
-			{
-				cols: [
-					{ gravity: 0.5 },
-					{
-						view: "button",
-						id: "with_google",
-						value: "Google",
-						css: "webix_primary",
-						gravity: 1,
-						css: "center",
-						autowidth: true,
-					},
-					{ gravity: 1.5 },
-					{
-						view: "button",
-						id: "with_github",
-						value: "Github",
-						css: "webix_primary",
-						align: "center",
-						gravity: 1,
-						autowidth: true,
-					},
-					{ gravity: 0.5 },
+					{ gravity: 1 },
 				],
 			},
 		],
 	};
+	var panel = {
+		view: "template",
+		template: "<div id='login-bg'></div>",
+		css: "login-bg",
+		id: "panel",
+	};
+	webix.ui({
+		master: "panel",
+		view: "template",
+		css: "popup",
+		id: "my-popup",
+
+		template: `<h5>Questions or Feedbacks?</h5>
+	         <div>
+	             If you have any questions about the test, look it up in our
+	             guide.
+	         </div>
+	         <a
+	             href="https://docs.massbit.io/about/massbit-intro"
+	             target="_blank"
+	         >
+	             See guide to test
+	         </a>
+	         <div id="or">
+	             <hr />
+	             Or
+	             <hr />
+	         </div>
+	         <h6>
+	             Any feedback? Feel free to reach out to us
+	         </h6>`,
+	});
+
+	webix.ui({
+		master: "panel",
+		view: "template",
+		css: "popup-area",
+		template:
+			"<button id='popup-button'><img src='assets/images/icons/LoginPopupIcon.png' alt='popup button'></img></button>",
+		width: 80,
+		height: 80,
+	});
+
 	var _layout = {
-		cols: [{}, _form, {}],
+		cols: [_form, panel],
 	};
 
 	return {
@@ -104,6 +178,14 @@ define([], function () {
 				window.open("/sso/github/app?redirect_uri=/app");
 			});
 			$$("register").attachEvent("onItemClick", function () {
+				window.location.href =
+					"https://dev.massbitroute.com/app/#!/auth/register";
+			});
+			$$("reset-password").attachEvent("onItemClick", function () {
+				window.location.href =
+					"https://dev.massbitroute.com/app/#!/auth/reset-password";
+			});
+			$$("login").attachEvent("onItemClick", function () {
 				var _email = $$("email").getValue();
 				var _password = $$("password").getValue();
 				webix
@@ -127,6 +209,9 @@ define([], function () {
 								});
 						}
 					});
+			});
+			$$("popup-button").attachEvent("onItemClick", function () {
+				console.log("popup");
 			});
 		},
 	};
--- apps/app/public/app/views/register.js
+++ apps/app/public/app/views/register.js
@@ -9,84 +9,103 @@ define([], function () {
 		},
 		elements: [
 			{
+				css: "px-2",
 				cols: [
+					{ gravity: 1 },
 					{
-						cols: [
+						gravity: 2,
+						rows: [
+							{
+								cols: [
+									{
+										cols: [
+											{
+												view: "template",
+												template:
+													'<img id="icon" src="assets/images/logo/logo.png" alt="MBR logo"></img>',
+												css: "icon",
+												gravity: 1,
+											},
+											{
+												view: "label",
+												label: "MassbitRoute",
+												css: "no-border",
+												gravity: 3,
+											},
+										],
+										gravity: 1,
+									},
+
+									{
+										view: "label",
+										label: "Docs",
+										css: "no-border",
+										align: "right",
+										gravity: 2,
+									},
+								],
+							},
+							{ gravity: 0.5 },
 							{
 								view: "template",
 								template:
-									'<img id="icon" src="assets/images/logo/logo.png" alt="MBR logo"></img>',
-								css: "icon",
-								gravity: 1,
+									"<h1 id='register-header'>Register</h1>",
+								align: "center",
+								height: 70,
+								borderless: true,
 							},
 							{
-								view: "label",
-								label: "MassbitRoute",
-								css: "no-border",
-								gravity: 3,
+								view: "button",
+								id: "login",
+								css: "no-bg webix_transparent",
+								value: "Already have an account? Login",
+								align: "center",
+								borderless: true,
 							},
+							{ view: "text", id: "username", label: "Username" },
+							{ view: "text", id: "email", label: "Email" },
+							{
+								view: "text",
+								id: "password",
+								type: "password",
+								label: "Password",
+							},
+							{
+								cols: [
+									{
+										view: "button",
+										id: "register",
+										value: "Signup",
+										css: "webix_primary",
+										align: "center",
+									},
+								],
+							},
+							{
+								cols: [
+									{
+										view: "button",
+										id: "with_google",
+										value: "Google",
+										css: "webix_transparent no-bg",
+										gravity: 1,
+										css: "center",
+									},
+									{ gravity: 0.2 },
+									{
+										view: "button",
+										id: "with_github",
+										value: "Github",
+										css: "webix_transparent no-bg",
+										align: "center",
+										gravity: 1,
+									},
+								],
+							},
+							{ gravity: 1 },
 						],
-						gravity: 1,
 					},
-
-					{
-						view: "label",
-						label: "Docs",
-						css: "no-border",
-						align: "right",
-						gravity: 2,
-					},
-				],
-			},
-			{
-				view: "template",
-				template: "<h1 id='register-header'>Register</h1>",
-				align: "center",
-				height: 80,
-				borderless: true,
-			},
-			{ view: "text", id: "username", label: "Username" },
-			{ view: "text", id: "email", label: "Email" },
-			{
-				view: "text",
-				id: "password",
-				type: "password",
-				label: "Password",
-			},
-			{
-				cols: [
-					{
-						view: "button",
-						id: "register",
-						value: "Register",
-						css: "webix_primary",
-						align: "center",
-					},
-				],
-			},
-			{
-				cols: [
-					{ gravity: 0.5 },
-					{
-						view: "button",
-						id: "with_google",
-						value: "Google",
-						css: "webix_primary",
-						gravity: 1,
-						css: "center",
-						autowidth: true,
-					},
-					{ gravity: 1.5 },
-					{
-						view: "button",
-						id: "with_github",
-						value: "Github",
-						css: "webix_primary",
-						align: "center",
-						gravity: 1,
-						autowidth: true,
-					},
-					{ gravity: 0.5 },
+					{ gravity: 1 },
 				],
 			},
 		],
@@ -104,6 +123,10 @@ define([], function () {
 			});
 			$$("with_github").attachEvent("onItemClick", function () {
 				window.open("/sso/github/app?redirect_uri=/app");
+			});
+			$$("login").attachEvent("onItemClick", function () {
+				window.location.href =
+					"https://dev.massbitroute.com/app/#!/auth/login";
 			});
 			$$("register").attachEvent("onItemClick", function () {
 				var _username = $$("username").getValue();
